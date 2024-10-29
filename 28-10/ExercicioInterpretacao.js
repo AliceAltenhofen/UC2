ba3c14456b7d1a472e4041adfeec05af3244d977
AliceAltenhofen <alicealtenhofendasilva.27@gmail.com>
2024-10-28 22:51:59 -0300

    Update ExercicioInterpretacao.js

--- 28-10/ExercicioInterpretacao.js
+++ 28-10/ExercicioInterpretacao.js
@@ -1,8 +1,8 @@
-let array
-console.log('a. ', array) //Imprime: a.
+let array //nao especifica o que contem a variavel array
+console.log('a. ', array) //Imprime: a. undefined
 
-array = null
-console.log('b. ', array) //Imprime: b. 
+array = null //esta dizendo que array agora é null
+console.log('b. ', array) //Imprime: b. null
 
 array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] 
 console.log('c. ', array.length)  //Imprime: c.11 
@@ -13,5 +13,5 @@ console.log('d. ', array[i]) // Imprime: 'd. 3
 array[i+1] = 19  //esta substituindo o numero do indice 1 por 19
 console.log('e. ', array) //Imprime: e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13] 
 
-const valor = array[i+6] 
-console.log('f. ', valor) //Imprime: f. 10
+const valor = array[i+6] // i=0 + 6 = 6(indice)
+console.log('f. ', valor) //Imprime: f. 9
